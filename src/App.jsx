d378b752ecc0ab8c039d4b565b595ce90b8c551d
Paula Bejarano <paubejarano2@gmail.com>
2022-11-02 17:38:48 -0300

    primeros cambios para empezar a usar React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1 id='titulo'>Hello World</h1>
+      <p id='descripcion'>Esta es mi primera pagina de React!</p>
+    </>
+  );
+}
+
+export default App;
